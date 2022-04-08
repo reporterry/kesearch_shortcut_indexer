8721412556d5c2e123bec053ebeba59530797612
Simon Ueding <ueding@eworks.de>
2022-04-08 13:22:56 +0200

    TYPO3 11.5 compatibility;

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,14 +4,14 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'KeSearch Indexer for Shortcut Elements',
     'description' => 'Indexer for shortcut elements, can handle mask records as well',
     'category' => 'be',
-    'version' => '1.0.0',
+    'version' => '1.1.0',
     'state' => 'stable',
     'author' => 'Henrik Ziegenhain',
     'author_email' => 'henrik@hziegenhain.de',
     'author_company' => '',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.4.99',
+            'typo3' => '9.5.0-11.5.99',
             'ke_search' => '3.1.0-3.99.99',
         ],
         'conflicts' => [],
